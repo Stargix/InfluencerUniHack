f87ab0e573e77501b2041b0a5c54c30b90c44248
Stargix <11sergifm@gmail.com>
2024-11-09 18:32:38 +0100

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 export default defineConfig({
   server: {
     port: 8080, // Change the port to 8080
   },
+
+  integrations: [tailwind()],
 });
